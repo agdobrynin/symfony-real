8dfce9156916bba6e135b72f455cb6dc37228adc
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-10-11 17:36:27 +0400

    Understanding Twig extension and using custom filter

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('text_by_percent', [$this, 'textByPercent']),
+        ];
+    }
+
+    public function textByPercent(string $text, int $percent = 50): string
+    {
+        if ($percent > 1 && $percent <= 100) {
+            $lenSource = mb_strlen($text);
+            $lenPercent = (int)($lenSource * $percent / 100);
+
+            return mb_substr($text, 0, $lenPercent);
+        }
+
+        $message = sprintf('Percent value of part text must be between 1 and 100. Yor values is %s', $percent);
+
+        throw new \UnexpectedValueException($message);
+    }
+}
