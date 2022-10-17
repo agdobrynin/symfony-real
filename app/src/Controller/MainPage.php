809d27f2541ba885160c73d17dcec36d9d082a0e
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-10-17 14:05:35 +0400

    Add main page controller and main page template.

---
+++ app/src/Controller/MainPage.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainPage extends AbstractController
+{
+    /**
+     * @Route("/", methods={"get"}, name="main_page")
+     */
+    public function index(): Response
+    {
+        return $this->render('main_page.html.twig');
+    }
+}
