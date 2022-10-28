fcbf2b1b426e5b33fb5b1e0da40978e7e240726b
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-10-28 11:49:53 +0400

    New design for Notification block 🔔 in header

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,8 +1,15 @@
 import "jquery/dist/jquery.slim";
-import "bootstrap/dist/js/bootstrap.min";
+import "@popperjs/core/dist/umd/popper.min";
+import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle.min";
+
+window.bootstrap = bootstrap;
 
 document.addEventListener("DOMContentLoaded", () => {
-    const notificationContainer = document.querySelector("#notification-main");
+    // Popover init
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+    const notificationContainer = document.querySelector("#notification");
 
     if (notificationContainer) {
         const text = notificationContainer.querySelector("#notification-text");
@@ -10,10 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const check = () => {
             $.get("/micro-post/notification/unread-all", (data) => {
-                if (loader) {
-                    loader.remove();
+                if (!loader.classList.contains("visually-hidden")) {
+                    loader.classList.add("visually-hidden");
                 }
-                text.textContent = data.all;
+
+                const notificationCount = data.all;
+                text.textContent = notificationCount;
+
+                if (notificationCount === 0) {
+                    text.classList.add("visually-hidden");
+                    notificationContainer.classList.add("grayscale");
+                } else {
+                    text.classList.remove("visually-hidden");
+                    notificationContainer.classList.remove("grayscale");
+                }
+
                 setTimeout(check, 5000);
             });
         };
