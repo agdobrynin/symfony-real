87fa9a6bc2e75e9cfc23f3fcb016c57a0659d110
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-04 12:16:09 +0400

    Testing: unit test for service Locales.

---
+++ app/tests/Service/MicroPost/LocalesTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Service\MicroPost;
+
+use App\Service\MicroPost\Locales;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class LocalesTest extends KernelTestCase
+{
+    public function testLocales(): void
+    {
+        $kernel = self::bootKernel();
+        $this->assertSame('test', $kernel->getEnvironment());
+
+        $locales = static::getContainer()->get(Locales::class);
+        $this->assertInstanceOf(Locales::class, $locales);
+
+        $configLocales = static::getContainer()->getParameter('app.supported_locales');
+        $this->assertEquals($configLocales, implode('|', $locales->getLocales()));
+        $this->assertStringStartsWith($locales->getDefaultLocale() . '|', $configLocales);
+    }
+}
