e332bb1230066d2a0d9b71a418ab431f3d3efaab
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-09 21:02:31 +0400

    Update LoginFormType - add constraints for login, password

--- app/src/Form/LoginFormType.php
+++ app/src/Form/LoginFormType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class LoginFormType extends AbstractType
 {
@@ -15,9 +17,17 @@ class LoginFormType extends AbstractType
         $builder
             ->add('_username', TextType::class, [
                 'label' => 'login_form.form.login.label',
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 3])
+                ]
             ])
             ->add('_password', PasswordType::class, [
                 'label' => 'login_form.form.login.password',
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 6])
+                ]
             ])
             ->add('save', SubmitType::class, [
                 'label' => 'login_form.form.login.button'
