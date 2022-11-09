2dadfc08da9518c3505a08a9d80f3940f8478706
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-09 10:39:36 +0400

    Test - update UserCheckerTest

--- app/tests/Unit/Security/UserCheckerTest.php
+++ app/tests/Unit/Security/UserCheckerTest.php
@@ -7,31 +7,65 @@ use App\Security\UserChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserCheckerTest extends TestCase
 {
+    protected function getCustomUser(): UserInterface
+    {
+        return new class implements UserInterface {
+            public function getRoles()
+            {
+            }
+
+            public function getPassword()
+            {
+            }
+
+            public function getSalt()
+            {
+            }
+
+            public function eraseCredentials()
+            {
+            }
+
+            public function getUsername()
+            {
+            }
+
+            public function __call($name, $arguments)
+            {
+            }
+        };
+    }
+
     public function getUsers(): \Generator
     {
-        yield [(new User())->setIsActive(false)];
-        yield [(new User())->setIsActive(true)];
+        yield [new User(), false];
+        yield [new User(), true];
+        yield [$this->getCustomUser(), null];
     }
 
     /**
      * @dataProvider getUsers
      */
-    public function testCheckPreAuth(User $user): void
+    public function testCheckPreAuth(UserInterface $user, ?bool $isActive): void
     {
         $em = self::createMock(EntityManagerInterface::class);
 
         $userChecker = (new UserChecker($em));
 
-        if (!$user->getIsActive()) {
-            $this->expectException(CustomUserMessageAccountStatusException::class);
-        } else {
-            $this->expectNotToPerformAssertions();
+        if ($user instanceof User && null !== $isActive) {
+            $user->setIsActive($isActive);
+
+            if (false === $isActive) {
+                self::expectException(CustomUserMessageAccountStatusException::class);
+            }
         }
 
         $userChecker->checkPreAuth($user);
+        self::expectNotToPerformAssertions();
     }
 
     public function testCheckPostAuth(): void
