3df9af76c2822dcc9abaf4eac4a626b094524e7b
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-09 18:04:57 +0400

    Refactoring - remove unused parameter from constructor ProfileFormType

--- app/src/Form/ProfileFormType.php
+++ app/src/Form/ProfileFormType.php
@@ -11,18 +11,15 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProfileFormType extends AbstractType
 {
     private $locales;
-    private $requestStack;
 
-    public function __construct(LocalesInterface $locales, RequestStack $requestStack)
+    public function __construct(LocalesInterface $locales)
     {
         $this->locales = $locales;
-        $this->requestStack = $requestStack;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
