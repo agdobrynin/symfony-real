70a7a777d8c1e9ce56e1c1e2e0fb9d152d945a8b
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-09 13:11:02 +0400

    Test for LoginType refactoring

--- app/tests/Unit/Form/LoginFormTypeTest.php
+++ app/tests/Unit/Form/LoginFormTypeTest.php
@@ -8,17 +8,27 @@ use Symfony\Component\Form\Test\TypeTestCase;
 
 class LoginFormTypeTest extends TypeTestCase
 {
+    protected const FORM_DATA = [
+        '_username' => 'test',
+        '_password' => 'pass',
+    ];
+
     public function testLoginForm(): void
     {
-        $formData = [
-            '_username' => 'test',
-            '_password' => 'pass',
-        ];
-
         $form = $this->factory->create(LoginFormType::class);
-        $form->submit($formData);
+        $form->submit(self::FORM_DATA);
 
         $this->assertTrue($form->isSynchronized());
-        self::assertEquals($formData, $form->getData());
+        self::assertEquals(self::FORM_DATA, $form->getData());
+    }
+
+    public function testLoginFormView(): void
+    {
+        $form = $this->factory->create(LoginFormType::class);
+        $fields = $form->createView()->children;
+
+        foreach (array_keys(self::FORM_DATA) as $item) {
+            self::assertArrayHasKey($item, $fields);
+        }
     }
 }
