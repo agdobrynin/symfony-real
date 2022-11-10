43c02ae1c835c6e3961f10f9afb136baf20db967
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-10 10:17:50 +0400

    Test update RegistrationFormType - add test for password not match.

--- app/tests/Unit/Form/RegistrationFormTypeTest.php
+++ app/tests/Unit/Form/RegistrationFormTypeTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Form;
 
 use App\Entity\User;
+use App\Entity\UserPreferences;
 use App\Form\RegistrationFormType;
 use App\Service\MicroPost\Locales;
 use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
@@ -12,6 +13,7 @@ use Symfony\Component\Form\Test\TypeTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Validator\Validation;
+use TypeError;
 
 class RegistrationFormTypeTest extends TypeTestCase
 {
@@ -22,6 +24,24 @@ class RegistrationFormTypeTest extends TypeTestCase
         'emoji',
         'password',
         'locale',
+    ];
+
+    protected const FORM_DATA_GOOD = [
+        'login' => 'myLogin',
+        'nick' => 'My Nick',
+        'email' => 'good@email.com',
+        'emoji' => '😀',
+        'password' => ['first' => 'myPassword', 'second' => 'myPassword'],
+        'locale' => 'en',
+    ];
+
+    protected const FORM_DATA_BAD_PASSWORD_NOT_MATCH = [
+        'login' => 'myLogin',
+        'nick' => 'My Nick',
+        'email' => 'good@email.com',
+        'emoji' => '😀',
+        'password' => ['first' => 'password-one', 'second' => 'password-two'],
+        'locale' => 'ru',
     ];
 
     public function testRegistrationFormTypeEmpty(): void
@@ -35,11 +55,43 @@ class RegistrationFormTypeTest extends TypeTestCase
 
     public function testRegistrationFormTypeFields(): void
     {
-        $form = $this->factory->create(RegistrationFormType::class);
+        $form = $this->factory->create(RegistrationFormType::class, new User());
         $fields = $form->createView()->children;
         foreach (self::FIELDS_KEY as $item) {
             self::assertArrayHasKey($item, $fields);
         }
+    }
+
+    public function testRegistrationFormTypeSuccess(): void
+    {
+        $user = new User();
+        $form = $this->factory->create(RegistrationFormType::class, $user);
+        $form->submit(self::FORM_DATA_GOOD);
+        self::assertTrue($form->isSynchronized());
+        $user->setPreferences((new UserPreferences())->setLocale($form->get('locale')->getData()));
+
+        self::assertEquals(self::FORM_DATA_GOOD['login'], $user->getLogin());
+        self::assertEquals(self::FORM_DATA_GOOD['nick'], $user->getNick());
+        self::assertEquals(self::FORM_DATA_GOOD['email'], $user->getEmail());
+        self::assertEquals(self::FORM_DATA_GOOD['emoji'], $user->getEmoji());
+        self::assertEquals(self::FORM_DATA_GOOD['locale'], $user->getPreferences()->getLocale());
+
+        // Password field is RepeatedType class
+        self::assertEquals(self::FORM_DATA_GOOD['password']['first'], $form->get('password')->getData());
+        // Password not set because form contain two fields with password - main and repeated.
+        self::expectException(TypeError::class);
+        $user->getPassword();
+    }
+
+    public function testRegistrationFormTypeFailPasswordNotMatch(): void
+    {
+        $user = new User();
+        $form = $this->factory->create(RegistrationFormType::class, $user);
+        $form->submit(self::FORM_DATA_BAD_PASSWORD_NOT_MATCH);
+        self::assertTrue($form->isSynchronized());
+
+        // Password field is RepeatedType class
+        self::assertNull($form->get('password')->getData());
     }
 
     protected function getExtensions(): array
