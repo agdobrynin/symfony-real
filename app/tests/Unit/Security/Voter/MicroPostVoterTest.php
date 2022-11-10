b8033fe37eb97f9339f11db2a59a2e00a0bcfbfc
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-10 18:44:46 +0400

    Test - update MicroPostVoterTest.

--- app/tests/Unit/Security/Voter/MicroPostVoterTest.php
+++ app/tests/Unit/Security/Voter/MicroPostVoterTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class MicroPostVoterTest extends TestCase
 {
@@ -17,7 +18,7 @@ class MicroPostVoterTest extends TestCase
         $userAdmin->setRoles([User::ROLE_ADMIN]);
 
         $token = self::createMock(TokenInterface::class);
-        $token->expects(self::never())->method('getUser');
+        $token->expects(self::once())->method('getUser')->willReturn($userAdmin);
 
         $accessDecisionManager = self::createMock(AccessDecisionManagerInterface::class);
         $accessDecisionManager->expects(self::once())
@@ -63,6 +64,60 @@ class MicroPostVoterTest extends TestCase
         $accessDecisionManager->expects(self::once())
             ->method('decide')->with($token)
             ->willReturn(false);
+
+        $vote = (new MicroPostVoter($accessDecisionManager))
+            ->vote($token, $this->createMicroPost($userPostOwner), [MicroPostVoter::EDIT_DEL_OWNER_OR_ADMIN]);
+
+        self::assertEquals(VoterInterface::ACCESS_DENIED, $vote);
+    }
+
+    public function testMicroPostVoterForCustomUserFail(): void
+    {
+        $customUser = new class implements UserInterface {
+
+            public function getUserIdentifier()
+            {
+                return 'super-admin';
+            }
+
+            public function getRoles()
+            {
+                return [User::ROLE_ADMIN];
+            }
+
+            public function getPassword()
+            {
+                return null;
+            }
+
+            public function getSalt()
+            {
+                return null;
+            }
+
+            public function eraseCredentials()
+            {
+            }
+
+            public function getUsername()
+            {
+                return 'super-admin';
+            }
+
+            public function __call($name, $arguments)
+            {
+            }
+        };
+
+        $token = self::createMock(TokenInterface::class);
+        $token->expects(self::once())->method('getUser')->willReturn($customUser);
+
+        $accessDecisionManager = self::createMock(AccessDecisionManagerInterface::class);
+        $accessDecisionManager->expects(self::never())
+            ->method('decide');
+
+        $userPostOwner = new User();
+        $userPostOwner->setRoles([User::ROLE_USER]);
 
         $vote = (new MicroPostVoter($accessDecisionManager))
             ->vote($token, $this->createMicroPost($userPostOwner), [MicroPostVoter::EDIT_DEL_OWNER_OR_ADMIN]);
