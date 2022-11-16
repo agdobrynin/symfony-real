35f2f1bf716d1956697a8821adb4ae428b18c5d7
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-16 12:26:09 +0400

    Refactoring - make exception message more informative.

--- app/src/Dto/MicroPostWithPaginationDto.php
+++ app/src/Dto/MicroPostWithPaginationDto.php
@@ -17,7 +17,9 @@ class MicroPostWithPaginationDto
     public function __construct(array $posts, PaginatorDto $paginatorDto)
     {
         if (count($posts) && (!$posts[0] instanceof MicroPost)) {
-            throw new MicroPostWithPaginationDtoException(sprintf('Params posts includes only %s objects', MicroPost::class));
+            $message = sprintf('Params posts includes only "%s" objects. Got "%s"', MicroPost::class, \get_class($posts[0]));
+
+            throw new MicroPostWithPaginationDtoException($message);
         }
 
         $this->posts = $posts;
