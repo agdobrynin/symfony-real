22ce9a3b727477710870d148586c95bd83ef1ad1
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-19 11:48:17 +0400

    Test for MicroPostCommentsWithPaginatorDto

---
+++ app/tests/Unit/Dto/MicroPostCommentsWithPaginatorDtoTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Dto;
+
+use App\Dto\Exception\MicroPostCommentsWithPaginatorDtoException;
+use App\Dto\MicroPostCommentsWithPaginatorDto;
+use App\Dto\PaginatorDto;
+use App\Entity\Comment;
+use PHPUnit\Framework\TestCase;
+
+class MicroPostCommentsWithPaginatorDtoTest extends TestCase
+{
+    public function sourceData(): \Generator
+    {
+        yield [[new Comment()], new PaginatorDto(1, 1, 1), null];
+        yield [[new class {
+        }], new PaginatorDto(1, 1, 1), MicroPostCommentsWithPaginatorDtoException::class];
+    }
+
+    /**
+     * @dataProvider sourceData
+     */
+    public function testMicroPostCommentsWithPaginatorDto(array $comments, PaginatorDto $paginatorDto, ?string $expectExceptionClass): void
+    {
+        if ($expectExceptionClass) {
+            self::expectException($expectExceptionClass);
+        }
+
+        $dto = new MicroPostCommentsWithPaginatorDto($comments, $paginatorDto);
+        self::assertTrue($dto->getPaginatorDto() instanceof PaginatorDto);
+
+        if (count($dto->getComments())) {
+            self::assertTrue(($dto->getComments()[0] ?? []) instanceof Comment);
+        }
+    }
+}
