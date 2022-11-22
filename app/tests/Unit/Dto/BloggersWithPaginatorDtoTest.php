e8d3fd6dbfb0e545b9926d2c8cd4f229556512cd
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-11-22 17:51:41 +0400

    Unit test for BloggersWithPaginatorDto, GetBloggersService

---
+++ app/tests/Unit/Dto/BloggersWithPaginatorDtoTest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Dto;
+
+use App\Dto\BloggersWithPaginatorDto;
+use App\Dto\Exception\BloggersWithPaginatorDtoException;
+use App\Dto\PaginatorDto;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class BloggersWithPaginatorDtoTest extends TestCase
+{
+    public function sourceData(): \Generator
+    {
+        yield [$this->getUserCollection(11), new PaginatorDto(1, 11, 10), null];
+        yield [[new class {
+        }], new PaginatorDto(1, 1, 1), BloggersWithPaginatorDtoException::class];
+    }
+
+    /**
+     * @dataProvider sourceData
+     */
+    public function testBloggersWithPagination(array $bloggers, PaginatorDto $paginatorDto, ?string $expectException): void
+    {
+        if ($expectException) {
+            self::expectException($expectException);
+        }
+
+        $dto = new BloggersWithPaginatorDto($bloggers, $paginatorDto);
+        self::assertSame($bloggers, $dto->getBloggers());
+        self::assertEquals($paginatorDto->getPage(), $dto->getPaginatorDto()->getPage());
+        self::assertEquals($paginatorDto->getPageSize(), $dto->getPaginatorDto()->getPageSize());
+        self::assertEquals($paginatorDto->getTotalPages(), $dto->getPaginatorDto()->getTotalPages());
+    }
+
+    protected function getUserCollection(int $count): array
+    {
+        $a = [];
+
+        for ($i = 0; $i < $count; $i++) {
+            $a[] = new User();
+        }
+
+        return $a;
+    }
+}
